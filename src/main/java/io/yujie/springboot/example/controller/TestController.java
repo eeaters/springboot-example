c304733ca30fb3a53e7d7a91ad5ab4d85e4308e1
yjw <yujie.wan.sh@weltown.cn>
2023-07-06 15:10:25 +0800

    docs(doc): doc-v3

--- src/main/java/io/yujie/springboot/example/controller/TestController.java
+++ src/main/java/io/yujie/springboot/example/controller/TestController.java
@@ -1,5 +1,9 @@
 package io.yujie.springboot.example.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import io.yujie.springboot.example.config.exception.LogicException;
 import io.yujie.springboot.example.entity.base.BaseResponse;
 import lombok.Data;
@@ -12,11 +16,12 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/test")
+@Tag(name = "测试类")
 public class TestController {
 
 
     @GetMapping("/ping")
-    public BaseResponse<String> test(@RequestParam String ping) {
+    public BaseResponse<String> test(@Parameter(example = "123", description = "test ping") @RequestParam String ping) {
         if ("5".equals(ping)) {
             throw new LogicException("此处有异常");
         }
@@ -30,19 +35,19 @@ public class TestController {
     }
 
     @PostMapping("/user")
-    public BaseResponse<Map<String, String>> post(@RequestBody @Valid User user) {
-        return BaseResponse.success(HashMap.newHashMap(4));
+    @Operation(summary = "新增个用户" , description = "随便一个描述")
+    public BaseResponse<User> post(@RequestBody @Valid User user) {
+        return BaseResponse.success(user);
     }
 
 
-
-
-
     @Data
-    public static class User{
+    public static class User {
         @NotBlank
+        @Schema(title = "用户名", description = "这是一个用户名字段", example = "admin")
         private String userName;
         @NotBlank
+        @Schema(title = "密码")
         private String password;
     }
 
